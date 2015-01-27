ab9459561bd35698181750680fe00ecbd31314c5
qz515880 <1195933889@qq.com>
2015-01-27 17:07:35 +0800

    topmenu修改

--- TowerGame/Classes/TopMenu.h
+++ TowerGame/Classes/TopMenu.h
@@ -2,22 +2,24 @@
 #define _TOPMENU_H_
 #include <string>
 #include "cocos2d.h"
-#include "GameLayer.h"
 
 using namespace cocos2d;
-class GameLayer;
 
-class TopMenu : public Node{
+class TopMenu : public Node {
 public:
-	static TopMenu* create(GameLayer* layer);
-	virtual bool init(GameLayer* layer);
-
+	static TopMenu* getInstance();
+	virtual bool init();
+	CREATE_FUNC(TopMenu);
+	void updatePlayerGold(int gameGold);
+	void pauseGame();
+private:
+	static TopMenu* _instance;
+	TopMenu();
+	void LoadTopMenu();
 private:
 	Label* lifeValue;
 	Label* waveValue;
 	Label* playerGold;
-private:
-	void LoadTopMenu();
 
 };
 #endif
