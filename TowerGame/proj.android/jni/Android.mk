e1340cb451f43bbad0974a25b1da3ba7f3ea9aaf
qz515880 <1195933889@qq.com>
2015-02-03 09:46:40 +0800

    android编译

--- TowerGame/proj.android/jni/Android.mk
+++ TowerGame/proj.android/jni/Android.mk
@@ -12,7 +12,27 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/Audio.cpp\
+                   ../../Classes/BlinkTitle.cpp\
+                   ../../Classes/CallAndroidMethod.cpp\
+                   ../../Classes/Chinese.cpp\
+                   ../../Classes/Creep.cpp\
+                   ../../Classes/GameData.cpp\
+                   ../../Classes/GameHUD.cpp\
+                   ../../Classes/GameLayer.cpp\
+                   ../../Classes/GameOverLayer.cpp\
+                   ../../Classes/GameOverScene.cpp\
+                   ../../Classes/GameScene.cpp\
+                   ../../Classes/GameState.cpp\
+                   ../../Classes/LevelSelectLayer.cpp\
+                   ../../Classes/Projectile.cpp\
+                   ../../Classes/StartScene.cpp\
+                   ../../Classes/TopMenu.cpp\
+                   ../../Classes/Tower.cpp\
+                   ../../Classes/TowerDamage.cpp\
+                   ../../Classes/TowerSpeed.cpp\
+                   ../../Classes/Wave.cpp\
+                   ../../Classes/WayPoint.cpp\                    
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
